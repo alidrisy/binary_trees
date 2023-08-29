77c960b4026f5cb71ac9d34c77090adaaec55256
alidrisy <a.alidrisy6@gmail.com>
2023-08-30 02:25:06 +0300

    fix errors

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+
+size_t binary_tree_size(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect
 * @tree: is a pointer to the tree
@@ -23,4 +27,37 @@ if (x && (n == i))
 return (1);
 
 return (0);
+}
+
+/**
+* binary_tree_is_full - checks if a binary tree is full
+* @tree: is a pointer to the tree
+* Return: 1 or 0
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+if (tree && !tree->left && !tree->right)
+return (1);
+if (tree->left && tree->right)
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+else
+return (0);
+}
+
+/**
+* binary_tree_size - measures the size of a binary tree
+* @tree: is a pointer to the tree
+* Return: the tree size
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
 
 /**
