1015974d518b7da50808103b0e86e9212bc2cf02
alidrisy <a.alidrisy6@gmail.com>
2023-08-30 21:47:39 +0300

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,29 +1,24 @@
 #include "binary_trees.h"
 
-
 /**
-* trees_ancestor - finds the lowest common ancestor of two nodes
-* @first: is a pointer to the first node
-* @second: is a pointer to the second node
-* Return: a pointer to the lowest common ancestor
+* depth - measures the depth of a binary tree
+* @tree: is a pointer to the tree
+* Return: the tree depth
 */
 
-binary_tree_t *trees_ancestor(binary_tree_t *first, binary_tree_t *second)
+size_t depth(const binary_tree_t *tree)
 {
-if (first == NULL || second == NULL)
-return (NULL);
+int x = 0;
 
-if (first->parent == second)
-return (second);
-if (second->parent == first)
-return (first);
-if (first->parent == second->parent)
-return (second->parent);
+if (tree == NULL)
+return (0);
 
-trees_ancestor(first->parent, second);
-trees_ancestor(first, second->parent);
-
-return (NULL);
+while (tree->parent)
+{
+tree = tree->parent;
+x++;
+}
+return (x);
 }
 
 /**
@@ -35,16 +30,20 @@ return (NULL);
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-binary_tree_t *new;
+if (first == NULL || second == NULL)
+return (NULL);
 
-if if (first == second)
+if (first == second)
 return ((binary_tree_t*)first);
 
-new = trees_ancestor((binary_tree_t*) first, (binary_tree_t*)second);
-if (new == NULL)
-new = trees_ancestor((binary_tree_t*) first->parent, (binary_tree_t*) second);
-if (new == NULL)
-new = trees_ancestor((binary_tree_t*) first, (binary_tree_t*) second->parent);
+if (depth(first) > depth(second))
+{
+return (binary_trees_ancestor(first->parent, second));
+}
+if (depth(first) < depth(second))
+{
+return (binary_trees_ancestor(first, second->parent));
+}
 
-return (new);
+return (binary_trees_ancestor(first->parent, second->parent));
 }
