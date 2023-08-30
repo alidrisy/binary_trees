096b2714e297387a7e2a2a3b386ad77bef9c1531
alidrisy <a.alidrisy6@gmail.com>
2023-08-30 21:50:32 +0300

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -34,7 +34,7 @@ if (first == NULL || second == NULL)
 return (NULL);
 
 if (first == second)
-return ((binary_tree_t*)first);
+return ((binary_tree_t *) first);
 
 if (depth(first) > depth(second))
 {
