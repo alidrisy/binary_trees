72a6d01dd5f44c20dd441675309624c75cf626d0
alidrisy <a.alidrisy6@gmail.com>
2023-09-01 00:02:46 +0300

    BST Insert

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
