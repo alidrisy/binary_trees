634f92dbe7610a0b5fbcc86f6d72e4375d8f9716
alidrisy <a.alidrisy6@gmail.com>
2023-09-01 00:46:07 +0300

    BST - Search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -2,7 +2,7 @@
 
 /**
 * bst_search - searches for a value in a Binary Search Tree
-* @tree is a pointer to the root node of the BST to search
+* @tree:/ is a pointer to the root node of the BST to search
 * @value: is the value to search in the tree
 * Return: a pointer to the node containing a value equals to value.
 */
